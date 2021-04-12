be7afac254584e081e6d5bf41a392f875e726948
cortelas <cortelas1@yahoo.com>
2021-04-11 21:23:39 -0700

    length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,10 @@
 // Assignment code here
+function generatePassword(){
+  // Declaring valic characters
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+  var passLength = prompt("Please enter your desired password length", "8");
 
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
