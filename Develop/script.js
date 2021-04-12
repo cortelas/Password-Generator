95186067b6be005a42e9c46a31157c1538e8d723
cortelas <cortelas1@yahoo.com>
2021-04-11 21:46:42 -0700

    character type discrimination

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,35 @@
 // Assignment code here
 function generatePassword(){
-  // Declaring valic characters
-  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-  var passLength = prompt("Please enter your desired password length", "8");
   var passStr = "";
 
+  // Finding password length
+  var passLength = prompt("Please enter your desired password length", "8");
+
+  // Declaring valid characters
+  //var allChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+  var charLower = "abcdefghijklmnopqrstuvwxyz";
+  var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var charNumeric = "0123456789";
+  var charSpecial = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+  // Finding desired character types
+  var charType = prompt("please enter the character types you want in your password", "lowercase, uppercase, numeric, special");
+  var chars = "";
+
+  if(charType.includes("lowercase")){
+    chars += charLower;
+  }
+  if(charType.includes("uppercase")){
+    chars += charUpper;
+  }
+  if(charType.includes("numeric")){
+    chars += charNumeric;
+  }
+  if(charType.includes("special")){
+    chars += charSpecial;
+  }
+
+  // Builds password from valid characters
   for (let index = 0; index < passLength; index++) {
     passStr += chars.charAt(Math.floor(Math.random() * chars.length));
     
